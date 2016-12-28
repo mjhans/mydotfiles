f6791f194e5b13332066a3c9516ef7dfbbcf5635
mjhans <mjhans83@gmail.com>
2016-12-28 20:41:30 +0900

    자바 톰캣 아파치 install 스크립트 추가

---
+++ ubuntu1404/install_java7_tomcat7_apache2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt-add-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install -y oracle-java7-installer
+
+sudo apt-get install tomcat7
+
+sudo apt-get install apache2
